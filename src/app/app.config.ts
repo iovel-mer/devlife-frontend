5f72778eb2571e2c9f66448e0579357b1195fbd1
iovel-mer <ioveeel.mind@gmail.com>
2025-06-27 03:01:17 +0400

    Resolve app.config.ts merge conflict

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
-<<<<<<< HEAD
 import { provideHttpClient, withFetch } from '@angular/common/http';
-=======
-import { provideHttpClient } from '@angular/common/http';
->>>>>>> 14556de17e333e361ad227d71584c1e682aa569e
 import { provideRouter } from '@angular/router';
-
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-<<<<<<< HEAD
-    provideHttpClient(withFetch()),         
-=======
-    provideHttpClient(),         
->>>>>>> 14556de17e333e361ad227d71584c1e682aa569e
-    provideRouter(routes),       
+    provideHttpClient(withFetch()),
+    provideRouter(routes),
   ],
 };
+
