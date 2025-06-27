0703acee59789d479a3514af3cd8174a0febead2
iovel-mer <ioveeel.mind@gmail.com>
2025-06-27 04:04:53 +0400

    Resolve merge conflict in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,13 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-<<<<<<< HEAD
 import { provideHttpClient, withFetch } from '@angular/common/http';
-=======
-import { provideHttpClient } from '@angular/common/http';
->>>>>>> 14556de17e333e361ad227d71584c1e682aa569e
 import { provideRouter } from '@angular/router';
-
 
 import { App } from './app/app';
 import { routes } from './app/app.routes';
+import { bootstrapApplication } from '@angular/platform-browser';
 
 bootstrapApplication(App, {
   providers: [
-<<<<<<< HEAD
-    provideHttpClient(withFetch()),       
-=======
-    provideHttpClient(),       
->>>>>>> 14556de17e333e361ad227d71584c1e682aa569e
+    provideHttpClient(withFetch()),
     provideRouter(routes),
-  ]
+  ],
 });
